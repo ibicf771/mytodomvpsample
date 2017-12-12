f1a7626a188cd80d4a46b6c5a5701c7cfc94c1e8
yangsimin <181326622@qq.com>
2017-12-12 13:03:05 +0800

    databing分支提交

--- app/src/main/java/com/architecture/android/todo_mvp_sample/tasks/TasksFragment.java
+++ app/src/main/java/com/architecture/android/todo_mvp_sample/tasks/TasksFragment.java
@@ -8,8 +8,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -71,6 +69,10 @@ public class TasksFragment extends Fragment implements TasksContract.View{
                 mPresenter.addItem();
             }
         });
+    }
+
+    public void onAddButtonClick(){
+        mPresenter.addItem();
     }
 
     @Override
